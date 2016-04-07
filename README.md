08075034f419c64b6a8514c320edc1c2c81c2541
Evan Kale <iseedeadpixel1337@gmail.com>
2016-04-07 08:57:44 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RN42Config
+- Author: Evan Kale
+- Email: evankale91@gmail.com
+- Web: ISeeDeadPixel.com
+- Blog: evankale.blogspot.ca
+- YouTube: youtube.com/EvanKale91
+
+See a demo video here:
+https://youtu.be/UJaqHnPR-XE
+
+- An Arduino dev tool.
+- Establishes a oftware serial communication channel on digtal pins D2 and D3 at the baud rate of 9600.
---
+++ RN42Config.ino
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015 Evan Kale
+ * Email: EvanKale91@gmail.com
+ * Website: www.ISeeDeadPixel.com
+ *          www.evankale.blogspot.ca
+ *
+ * This file is part of RN42Config.
+ *
+ * RN42Config is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetoothSerial(2, 3); //(RX, TX)
+
+void setup()
+{
+  Serial.begin(9600);
+  bluetoothSerial.begin(9600);
+}
+
+void loop()
+{
+  if (bluetoothSerial.available())
+  {
+    Serial.print((char)bluetoothSerial.read());
+  }
+  if (Serial.available())
+  {
+    bluetoothSerial.print((char)Serial.read());
+  }
+}
+
